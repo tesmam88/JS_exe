159fccd6b804b8846f447a36e5aa26e5b605d174
Tesmam <tespereyra88@gmail.com>
2026-03-04 06:18:20 -0400

    exe de conversões en JS e Data types

---
+++ convertion.js
@@ -0,0 +1,9 @@
+let country = "Brazil";
+let continent = "South America"
+console.log(`You are in ${country}`, (typeof country))
+console.log(`You live in ${continent}`,(typeof continent))
+
+console.log(country.charAt(0))
+console.log(continent.length)
+console.log(continent.slice(6,9))
+console.log(continent.split(''))
