f55c03ef5e03ca86b286c53515be6ef5c77c3327
Tesmam <tespereyra88@gmail.com>
2026-03-04 05:50:08 -0400

    exe com declaração de variaveis

---
+++ listacontatos.js
@@ -0,0 +1,29 @@
+// declaração de variaveis
+    let name1= "Maxwell Wright";
+    let phone1= "(0191)719 695";
+    let email1="maxwellwright@email.co.uk";   
+     
+    let name2="Alice Johnson";
+    let phone2="(020)123 456";
+    let email2="alicejohnson@email.co.uk";
+    
+    let name3="Brian Smith";
+    let phone3="(030)987 654";
+    let email3="briansmith@email.co.uk";
+
+    console.log(name1);
+    console.log(phone1);
+    console.log(email1);
+
+    console.log(name2);
+    console.log(phone2);
+    console.log(email2);
+
+    console.log(name3);
+    console.log(phone3);
+    console.log(email3);
+    
+
+    
+    
+
