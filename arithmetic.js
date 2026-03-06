bc07a51b50ea367b0bd0e6ce9b4ccde4e120eb2b
Tesmam <119597523+tesmam88@users.noreply.github.com>
2026-03-06 15:28:52 -0400

    assignments n' arithmetics JS

---
+++ arithmetic.js
@@ -0,0 +1,13 @@
+// Arithmetic operators basic in JS
+
+const num1 = 5;
+const num2 = 3;
+
+console.log(num1+num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1-num2);
+console.log(num1+num2);
+console.log(num1%num2);
+console.log(num1**num2);
+
---
+++ assignments.js
@@ -0,0 +1,14 @@
+// Assignments operators and types
+x = 10;
+x += 2;
+console.log(x);
+x -= 2;
+console.log(x)
+x *= 2;
+console.log(x);
+x /= 2;
+console.log(x);
+x **=2;
+console.log(x);
+x %=5;
+console.log(x);
