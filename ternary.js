f024157faefd48659c7b99cd9d2311ad4cb527f3
Tesmam <tespereyra88@gmail.com>
2026-03-12 05:59:42 -0400

    simple ternary JS

---
+++ ternary.js
@@ -0,0 +1,14 @@
+//ternary operator JS
+
+console.log(true ? "Alice":"Bob");
+console.log(false ? "Alice":"Bob");
+
+console.log('---------------------')
+//ternary with some rules
+
+console.log(1 < 2 ? "Alice":"Bob");
+console.log(1 > 2 ? "Alice":"Bob");
+
+//bigger letter has smaller value on comparation with small letter
+console.log("ReD"<"red"?"Alice":"Bob");
+console.log("ReD">"red"?"Alice":"Bob");
