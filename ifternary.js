1170da596fb5f381227db376958312d19ffe3168
Tesmam <119597523+tesmam88@users.noreply.github.com>
2026-03-17 17:14:10 -0400

    example using conditional n' ternary

--- ifternary.js
+++ ifternary.js
@@ -3,4 +3,4 @@
 let price = {};
 price.user = Number(prompt("What's the price?",0));
 let shippingCost = price.user > 50 ? 0 : 5;
-console.log (`price: ${price} shippingCost : ${shippingCost}`);
+console.log (`price: ${price.user} shippingCost : ${shippingCost}`);
