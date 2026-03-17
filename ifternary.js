b909b28a95ff5b0c3c6c23e1b8836db74a99ffde
Tesmam <119597523+tesmam88@users.noreply.github.com>
2026-03-17 17:12:08 -0400

    Conditional with ternary

---
+++ ifternary.js
@@ -0,0 +1,6 @@
+// Using conditional with ternary aplication
+
+let price = {};
+price.user = Number(prompt("What's the price?",0));
+let shippingCost = price.user > 50 ? 0 : 5;
+console.log (`price: ${price} shippingCost : ${shippingCost}`);
