a751c55c61f75c96a650fa39a8e9d3d02afbcb92
tesmam88 <tespereyra88@gmail.com>
2026-03-18 20:17:35 -0400

    listcontactif done

--- listcontactif.js
+++ listcontactif.js
@@ -1,6 +1,4 @@
 
-let usermenu = prompt("Choose an option: a) first contact, b) last contact, c) add contact ")
-let newcontact = prompt("name: ,phone: ,email: ")
 let contacts = [{
     name: "Maxwell Wright",
     phone: "(0191) 719 6495",
@@ -15,16 +13,29 @@ let contacts = [{
     email: "libero@convallis.edu"
 }];
 
+let usermenu = prompt("Choose an option: a) first contact, b) last contact, c) add contact ")
+
 if (usermenu === 'a'){
-    alert(contacts[0])
+    alert(`${contacts[0].name} , ${contacts[0].phone} , ${contacts[0].email}`)
     console.log(contacts[0]);
 }
 else if(usermenu === 'b'){
-    alert(contacts.length -1)
-    console.log(contacts.length -1);
+    let last = contacts[contacts.length -1];
+    alert(`${last.name} , ${last.phone} , ${last.email}`)
+    console.log(last);
 }
-if (usermenu ==='c'){
-    newcontact = contacts.push()
-    console.log(newcontact);
-    alert(newcontact)
-}
+else if (usermenu ==='c'){
+    let name = prompt("put your name: ")
+    let phone = prompt("put you phone: ")
+    let email = prompt("put your email: ")
+
+    contacts.push({
+    name:name,
+    phone:phone,
+    email:email})
+    
+    console.log(contacts);
+    alert("add new contact")    
+
+};
+
