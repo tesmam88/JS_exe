1d4f8c91b6780f57da8c99d1b34fc8b12fb193c9
tesmam88 <tespereyra88@gmail.com>
2026-05-13 21:49:10 -0400

    js exercises
    alert,prompt,confitm

---
+++ confirm.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+    confirm("Hello Guys!!");
+</script>
---
+++ prompt.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+    prompt("Quantos anos vc tem???");
+    console.log(prompt("Holland"))
+</script>
